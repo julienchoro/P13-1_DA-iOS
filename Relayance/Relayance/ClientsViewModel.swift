10d4eff93d303a86d8d8746bbb76cf4e8d160c4f
julienchoro <julienchoro@icloud.com>
2025-07-27 18:52:19 +0200

    test(ClientsViewModel): BDD - Added supprimerClient valid case

--- Relayance/Relayance/ClientsViewModel.swift
+++ Relayance/Relayance/ClientsViewModel.swift
@@ -23,6 +23,12 @@ class ClientsViewModel: ObservableObject {
         }
     }
     
+    func supprimerClient(client: Client) -> Bool {
+        let index = clientsList.firstIndex(of:client)
+        clientsList.remove(at: index!)
+        return true
+    }
+    
     private func validerEmail(email: String) -> Bool {
         let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
         let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
--- Relayance/RelayanceTests/ClientsViewModelTests.swift
+++ Relayance/RelayanceTests/ClientsViewModelTests.swift
@@ -10,25 +10,46 @@ import XCTest
 
 final class ClientsViewModelTests: XCTestCase {
 
-    // Green
+    // Test for the Add client feature
     func test_ajouterClient_GivenValidEmailAndName_WhenAddingClient_ThenClientIsAdded() {
+        // Given
         let clientsViewModel = ClientsViewModel()
         let nom = "Ju Cho"
         let email = "julienchoro@icloud.com"
         
+        // When
         let success = clientsViewModel.ajouterNouveauClient(nom: nom, email: email)
         
+        // Then
         XCTAssertTrue(success)
     }
     
     func test_ajouterClient_GivenInvalidEmailAndName_WhenAddingClient_ThenClientIsNotAdded() {
+        // Given
         let clientsViewModel = ClientsViewModel()
         let nom = "Ju Cho"
         let email = "JuCho.invalide"
         
+        // When
         let success = clientsViewModel.ajouterNouveauClient(nom: nom, email: email)
 
+        // Then
         XCTAssertFalse(success)
+    }
+    
+    // Test for the delete client feature
+    func test_supprimerClient_GivenExistingClient_WhenDeletingClient_ThenClientIsRemoved() {
+        // Given
+        let viewModel = ClientsViewModel()
+        let fridaClient = viewModel.clientsList.first(where: { $0.nom == "Frida Kahlo" })
+        XCTAssertEqual(fridaClient?.nom, "Frida Kahlo" )
+        
+        // When
+        let success = viewModel.supprimerClient(client: fridaClient!)
+        
+        // Then
+        XCTAssertTrue(success)
+        XCTAssertFalse(viewModel.clientsList.contains(where: { $0.nom == "Frida Kahlo" }))
     }
 
 }
